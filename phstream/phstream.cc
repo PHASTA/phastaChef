dd321b3c35ab5206bb9bafb49bad14cf4ec6007b
Cameron Smith <smithc11@rpi.edu>
2015-08-18 22:34:32 -0400

    use typedef

--- phstream/phstream.cc
+++ phstream/phstream.cc
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string>
+#include <assert.h>
 #include "phstream.h" 
 
 extern "C" {
--- phstream/phstream.h
+++ phstream/phstream.h
@@ -1,32 +1,32 @@
 #ifndef PHSTREAM_H_
 #define PHSTREAM_H_
 #include <stdio.h>
-struct RStream;
-struct GRStream;
+typedef struct RStream* rstream;
+typedef struct GRStream* grstream;
 /** @brief make restart stream */
-RStream* makeRStream();
+rstream makeRStream();
 /** @brief clear restart stream */
-void clearRStream(RStream* rs);
+void clearRStream(rstream rs);
 /** @brief detach output stream */
-void destroyRStream(RStream* rs);
+void destroyRStream(rstream rs);
 
 /** @brief make geom-restart stream */
-GRStream* makeGRStream();
+grstream makeGRStream();
 /** @brief clear geom-restart stream */
-void clearGRStream(GRStream* grs);
+void clearGRStream(grstream grs);
 /** @brief destroy geom-restart stream */
-void destroyGRStream(GRStream* grs);
+void destroyGRStream(grstream grs);
 
 /** @brief open restart stream for reading*/
-FILE* openRStreamRead(RStream* rs);
+FILE* openRStreamRead(rstream rs);
 /** @brief open restart stream for writing*/
-FILE* openRStreamWrite(RStream* rs);
+FILE* openRStreamWrite(rstream rs);
 
 /** @brief open named stream in geom-restart stream for reading*/
-FILE* openGRStreamRead(GRStream* grs, const char* named);
+FILE* openGRStreamRead(grstream grs, const char* named);
 /** @brief open named stream in geom-restart stream for writing*/
-FILE* openGRStreamWrite(GRStream* grs, const char* named);
+FILE* openGRStreamWrite(grstream grs, const char* named);
 
 /** @brief dev function */
-void attachRStream(GRStream* grs, RStream* rs);
+void attachRStream(grstream grs, rstream rs);
 #endif 
