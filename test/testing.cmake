93f5cbcd85c567fd5f8419e0bbc8e8eb87a39972
Cameron Smith <smithc11@rpi.edu>
2015-08-20 10:11:06 -0400

    stream test has nothing to do with sync, add file based version of stream test

--- test/testing.cmake
+++ test/testing.cmake
@@ -1,9 +1,19 @@
 set(CDIR ${CASES}/incompressible)
+
+set(chef_phasta ${PHASTACHEF_BINARY_DIR}/chef_phasta)
+add_test(chefPhastaStream_copyInpCfg
+  cp ${PHASTA_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
+add_test(
+  NAME chefPhasta_incompressible
+  COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${chef_phasta}
+  WORKING_DIRECTORY ${CDIR}
+)
+
 set(chef_phasta_stream ${PHASTACHEF_BINARY_DIR}/chef_phasta_stream)
 add_test(chefPhastaStream_copyInpCfg
   cp ${PHASTA_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
 add_test(
-  NAME chefPhastaStream_incompressible-sync
+  NAME chefPhastaStream_incompressible
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${chef_phasta_stream}
   WORKING_DIRECTORY ${CDIR}
 )
@@ -13,7 +23,7 @@ set(cmd
   ${CDIR}/4-procs_case-SyncIO-2_ref/
   2 1e-6)
 add_test(
-  NAME chefPhastaStream_compareIncompressibleSync
+  NAME chefPhastaStream_compareIncompressible
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${cmd}
   WORKING_DIRECTORY ${CDIR}
 )
