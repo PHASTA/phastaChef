d54faa8ace064f35e44573c8f857915b4ebd84ca
Cameron Smith <smithc11@rpi.edu>
2015-08-24 20:03:02 -0400

    remove extra config copy and add comparison

--- test/testing.cmake
+++ test/testing.cmake
@@ -8,10 +8,18 @@ add_test(
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${chef_phasta}
   WORKING_DIRECTORY ${CDIR}
 )
+set(cmd
+  ${PHASTA_BINARY_DIR}/bin/checkphasta
+  ${CDIR}/4-procs_case/
+  ${CDIR}/4-procs_case-SyncIO-2_ref/
+  2 1e-6)
+add_test(
+  NAME chefPhasta_compareIncompressible
+  COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${cmd}
+  WORKING_DIRECTORY ${CDIR}
+)
 
 set(chef_phasta_stream ${PHASTACHEF_BINARY_DIR}/chef_phasta_stream)
-add_test(chefPhastaStream_copyInpCfg
-  cp ${PHASTA_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
 add_test(
   NAME chefPhastaStream_incompressible
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${chef_phasta_stream}
