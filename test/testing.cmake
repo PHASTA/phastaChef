a1632bc7d009bc53fcf764d34f8dd67c8baaefff
Cameron Smith <smithc11@rpi.edu>
2015-08-26 12:22:22 -0400

    rename the procs directory as tests are run

--- test/testing.cmake
+++ test/testing.cmake
@@ -3,6 +3,7 @@ set(CDIR ${CASES}/incompressible)
 set(chefPhasta_posix ${PHASTACHEF_BINARY_DIR}/chefPhasta_posix)
 add_test(chefPhastaStream_copyInpCfg
   cp ${PHASTA_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
+
 add_test(
   NAME chefPhasta_incompressible
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${chefPhasta_posix}
@@ -16,6 +17,11 @@ set(cmd
 add_test(
   NAME chefPhasta_compareIncompressible
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${cmd}
+  WORKING_DIRECTORY ${CDIR}
+)
+add_test(
+  NAME chefPhasta_mvProcsCaseDir
+  COMMAND mv ${CDIR}/4-procs_case ${CDIR}/4-procs_case_chefPhasta
   WORKING_DIRECTORY ${CDIR}
 )
 
@@ -35,10 +41,20 @@ add_test(
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${cmd}
   WORKING_DIRECTORY ${CDIR}
 )
+add_test(
+  NAME chefPhastaStream_mvProcsCaseDir
+  COMMAND mv ${CDIR}/4-procs_case ${CDIR}/4-procs_case_chefPhastaStream
+  WORKING_DIRECTORY ${CDIR}
+)
 
 set(chefPhastaChef_stream ${PHASTACHEF_BINARY_DIR}/chefPhastaChef_stream)
 add_test(
   NAME chefPhastaChefStream_incompressible
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${chefPhastaChef_stream}
+  WORKING_DIRECTORY ${CDIR}
+)
+add_test(
+  NAME chefPhastaChefStream_mvProcsCaseDir
+  COMMAND mv ${CDIR}/4-procs_case ${CDIR}/4-procs_case_chefPhastaChefStream
   WORKING_DIRECTORY ${CDIR}
 )
