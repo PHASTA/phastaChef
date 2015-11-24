aebf62202b84e2a64123a2d589ef1434b52e3ef2
Cameron Smith <smithc11@rpi.edu>
2015-11-23 21:30:47 -0500

    file-based adaptive loop test

--- chef_phasta_adaptLoop_files.cc
+++ chef_phasta_adaptLoop_files.cc
@@ -26,10 +26,24 @@ namespace {
     }
   }
 
-  std::string makeMeshName(int step, const char* prefix="") {
+  std::string makeMeshName(int step) {
     std::stringstream meshname;
-    meshname  << "bz2:" << prefix << "t" << step << "p" << PCU_Comm_Peers() << "/";
+    meshname  << "bz2:" << "t" << step << "p" << PCU_Comm_Peers() << "/";
     return meshname.str();
+  }
+
+  std::string makeRestartName() {
+    std::stringstream restartname;
+    restartname << PCU_Comm_Peers() << "-procs_case/restart";
+    return restartname.str();
+  }
+
+  std::string prefixCwd(std::string name) {
+    char cwd[4096] = "\0";
+    getcwd(cwd,4096);
+    std::stringstream s;
+    s << cwd << "/" << name;
+    return s.str();
   }
 
   void setupChef(ph::Input& ctrl, int step) {
@@ -49,6 +63,7 @@ namespace {
       ctrl.adaptFlag = 1;
     }
     ctrl.outMeshFileName = makeMeshName(step);
+    ctrl.restartFileName = makeRestartName();
   }
 
 }
@@ -67,13 +82,15 @@ int main(int argc, char** argv) {
   apf::Mesh2* m = 0;
   ph::Input ctrl;
   ctrl.load("adapt.inp");
+  ctrl.modelFileName = prefixCwd(ctrl.modelFileName);
+  ctrl.attributeFileName = prefixCwd(ctrl.attributeFileName);
   ctrl.outMeshFileName = makeMeshName(0);
   chef::cook(g,m,ctrl);
   freeMesh(m); m = NULL;
   phSolver::Input inp("solver.inp", "input.config");
   int step = 0;
   do {
-    ctrl.meshFileName = makeMeshName(step, "../");
+    ctrl.meshFileName = makeMeshName(step);
     step = phasta(inp);
     assert(step >= 0);
     if(!PCU_Comm_Self())
