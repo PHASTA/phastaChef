41c7acdda75e2b4d6582af74d5495a74e33c4a75
Cameron Smith <smithc11@rpi.edu>
2015-12-01 15:58:47 -0500

    sam based adaptive loop test

---
+++ chef_phasta_sam_adaptLoop.cc
@@ -0,0 +1,110 @@
+#include "chefPhasta.h"
+#include <PCU.h>
+#include <chef.h>
+#include <phasta.h>
+#include <phstream.h>
+#include <sam.h>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <assert.h>
+
+namespace {
+  void freeMesh(apf::Mesh* m) {
+    m->destroyNative();
+    apf::destroyMesh(m);
+  }
+
+  apf::Field* getField(apf::Mesh* m) {
+    /* if the value of the fldIdx'th index from the fldName
+     * field is greater than fldLimit then multiply the current
+     * isotropic mesh size at the vertex by szFactor */
+    const unsigned fldIdx = 5;
+    const double fldLimit = 1e-6;
+    const double szFactor = 0.5;
+    const char* fldName = "errors";
+    return sam::specifiedIso(m,fldName,fldIdx,fldLimit,szFactor);
+  }
+
+  static FILE* openfile_read(ph::Input&, const char* path) {
+    return fopen(path, "r");
+  }
+
+  static FILE* openstream_read(ph::Input& in, const char* path) {
+    std::string fname(path);
+    std::string restartStr("restart");
+    FILE* f = NULL;
+    if( fname.find(restartStr) != std::string::npos )
+      f = openRStreamRead(in.rs);
+    else {
+      fprintf(stderr,
+        "ERROR %s type of stream %s is unknown... exiting\n",
+        __func__, fname.c_str());
+      exit(1);
+    }
+    return f;
+  }
+
+  void setupChef(ph::Input& ctrl, int step) {
+    //don't split or tetrahedronize
+    ctrl.splitFactor = 1;
+    ctrl.recursivePtn = 0;
+    ctrl.tetrahedronize = 0;
+    ctrl.timeStepNumber = step;
+    ctrl.solutionMigration = 1;
+    if(step>1) {
+      if(!PCU_Comm_Self()) {
+        fprintf(stderr, "STATUS error based adapt %d\n", step);
+        fprintf(stderr, "STATUS ctrl.attributeFileName %s step %d\n",
+            ctrl.attributeFileName.c_str(), step);
+      }
+      ctrl.adaptStrategy = 1; //error field adapt
+      ctrl.adaptFlag = 1;
+    }
+  }
+}
+int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
+  PCU_Comm_Init();
+  PCU_Protect();
+  if( argc != 2 ) {
+    if(!PCU_Comm_Self())
+      fprintf(stderr, "Usage: %s <maxTimeStep>\n",argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  int maxStep = atoi(argv[1]);
+  chefPhasta::initModelers();
+  apf::Mesh2* m = 0;
+  grstream grs = makeGRStream();
+  ph::Input ctrl;
+  ctrl.load("adapt.inp");
+  /* setup file reading */
+  ctrl.openfile_read = openfile_read;
+  chef::readAndAttachFields(ctrl,m);
+  chef::preprocess(m,ctrl,grs);
+  rstream rs = makeRStream();
+  /* setup stream reading */
+  ctrl.openfile_read = openstream_read;
+  ctrl.rs = rs;
+  phSolver::Input inp("solver.inp", "input.config");
+  int step = 0;
+  do {
+    step = phasta(inp,grs,rs);
+    clearGRStream(grs);
+    if(!PCU_Comm_Self())
+      fprintf(stderr, "STATUS ran to step %d\n", step);
+    setupChef(ctrl,step);
+    chef::readAndAttachFields(ctrl,m);
+    apf::Field* szFld = getField(m);
+    assert(szFld);
+    chef::adapt(m,szFld);
+    chef::preprocess(m,ctrl,grs);
+    clearRStream(rs);
+  } while( step < maxStep );
+  destroyGRStream(grs);
+  destroyRStream(rs);
+  freeMesh(m);
+  chefPhasta::finalizeModelers();
+  PCU_Comm_Free();
+  MPI_Finalize();
+}
