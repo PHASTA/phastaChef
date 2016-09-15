1ff0448c7a25457876a363f0d5f8f194d30b7a92
Cameron Smith <smithc11@rpi.edu>
2016-09-15 12:02:12 -0400

    one test per run of solver

---
+++ test/runphasta.cmake
@@ -0,0 +1,27 @@
+macro(cmd dir exe)
+  message("${exe} ${ARGN}")
+  execute_process(
+    COMMAND ${exe} ${ARGN}
+    WORKING_DIRECTORY ${dir} 
+    OUTPUT_VARIABLE out
+    ERROR_VARIABLE out
+    RESULT_VARIABLE res
+    )
+  message("${out}")
+  if(res)
+    message(FATAL_ERROR "Error running ${exe}")
+  else()
+    message("Success")
+  endif()
+endmacro()
+
+cmd(${WORKDIR} cp ${INPFILE} ${WORKDIR})
+cmd(${WORKDIR} ${MPIRUN} ${MPIRUN_PROCFLAG} ${NUMPROCS} ${EXE} ${MAXSTEPS})
+message("running rm and mv")
+foreach(src ${SRCDIRS})
+  set(mname chefphasta_${NAME})
+  set(tname ${mname}_mv${src})
+  set(tgtdir ${WORKDIR}/${src}_${mname}) 
+  cmd(${WORKDIR} rm -rf ${tgtdir})
+  cmd(${WORKDIR} mv ${WORKDIR}/${src} ${tgtdir})
+endforeach()
--- test/testing.cmake
+++ test/testing.cmake
@@ -1,88 +1,94 @@
 set(testLabel "chefphasta")
-macro(cp_parallel_test name procs dir exe)
-  set(tname chefphasta_${name})
-  add_test(
-    NAME ${tname}
-    COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} ${procs} ${exe} ${ARGN}
-    WORKING_DIRECTORY ${dir} )
-  set_tests_properties(${tname} PROPERTIES LABELS ${testLabel})
-endmacro(cp_parallel_test)
-
-macro(cp_serial_test name exe)
-  set(tname chefphasta_${name})
-  add_test( NAME ${tname} COMMAND ${exe} ${ARGN} )
-  set_tests_properties(${tname} PROPERTIES LABELS ${testLabel})
-endmacro(cp_serial_test)
-
-macro(cp_move_dir name work src) 
-  set(mname chefphasta_${name})
-  set(tname ${mname}_mv${src})
-  set(tgtdir ${work}/${src}_${mname}) 
-  add_test(
-    NAME ${tname}
-    COMMAND rm -rf ${tgtdir} && mv ${work}/${src} ${tgtdir}
-    WORKING_DIRECTORY ${work})
-  set_tests_properties(${tname} PROPERTIES LABELS ${testLabel})
-endmacro(cp_move_dir)
-
 if( ${phastaIC_FOUND} )
   set(CDIR ${CASES}/incompressible)
-  cp_serial_test(copy_inpConfig
-    cp ${PHASTA_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
+  set(casename ${testLabel}_posix_incompressible)
+  add_test(NAME ${casename}
+    COMMAND ${CMAKE_COMMAND}
+    -DNAME=${casename}
+    -DWORKDIR=${CDIR}
+    -DINPFILE=${PHASTA_SOURCE_DIR}/phSolver/common/input.config
+    -DMPIRUN=${MPIRUN}
+    -DMPIRUN_PROCFLAG=${MPIRUN_PROCFLAG}
+    -DEXE=${PHASTACHEF_BINARY_DIR}/chefPhasta_posix
+    -DNUMPROCS=4
+    -DSRCDIRS=4-procs_case
+    -P ${CMAKE_CURRENT_SOURCE_DIR}/runphasta.cmake
+    )
   
-  set(exe ${PHASTACHEF_BINARY_DIR}/chefPhasta_posix)
-  set(casename posix_incompressible)
-  cp_parallel_test(${casename} 4 ${CDIR} ${exe})
-  cp_parallel_test(check_${casename} 4 ${CDIR}
-    ${PHASTA_BINARY_DIR}/bin/checkphasta
-    ${CDIR}/4-procs_case/
-    ${CDIR}/4-procs_case-PosixChefPhasta_ref/
-    0 1e-6)
-  cp_move_dir(${casename} ${CDIR} 4-procs_case)
+  set(casename ${testLabel}_stream_incompressible)
+  add_test(NAME ${casename}
+    COMMAND ${CMAKE_COMMAND}
+    -DNAME=${casename}
+    -DWORKDIR=${CDIR}
+    -DINPFILE=${PHASTA_SOURCE_DIR}/phSolver/common/input.config
+    -DMPIRUN=${MPIRUN}
+    -DMPIRUN_PROCFLAG=${MPIRUN_PROCFLAG}
+    -DEXE=${PHASTACHEF_BINARY_DIR}/chefPhasta_stream
+    -DNUMPROCS=4
+    -DSRCDIRS=4-procs_case
+    -P ${CMAKE_CURRENT_SOURCE_DIR}/runphasta.cmake
+    )
   
-  set(exe ${PHASTACHEF_BINARY_DIR}/chefPhasta_stream)
-  set(casename stream_incompressible)
-  cp_parallel_test(${casename} 4 ${CDIR} ${exe})
-  cp_parallel_test(check_${casename} 4 ${CDIR} 
-    ${PHASTA_BINARY_DIR}/bin/checkphasta
-    ${CDIR}/4-procs_case/
-    ${CDIR}/4-procs_case-SyncIO-2_ref/
-    2 1e-6)
-  cp_move_dir(${casename} ${CDIR} 4-procs_case)
-  
-  set(exe ${PHASTACHEF_BINARY_DIR}/chefPhastaLoop_stream_ur)
-  set(maxTimeStep 12)
-  set(casename loopStreamUR_incompressible)
-  cp_parallel_test(${casename} 4 ${CDIR} ${exe} ${maxTimeStep})
-  cp_move_dir(${casename} ${CDIR} 4-procs_case)
-  cp_move_dir(${casename} ${CDIR} 4)
+  set(casename ${testLabel}_loopStreamUR_incompressible)
+  add_test(NAME ${casename}
+    COMMAND ${CMAKE_COMMAND}
+    -DNAME=${casename}
+    -DWORKDIR=${CDIR}
+    -DINPFILE=${PHASTA_SOURCE_DIR}/phSolver/common/input.config
+    -DMPIRUN=${MPIRUN}
+    -DMPIRUN_PROCFLAG=${MPIRUN_PROCFLAG}
+    -DEXE=${PHASTACHEF_BINARY_DIR}/chefPhastaLoop_stream_ur
+    -DNUMPROCS=4
+    -DMAXSTEPS=12
+    -DSRCDIRS=4-procs_case$<SEMICOLON>4
+    -P ${CMAKE_CURRENT_SOURCE_DIR}/runphasta.cmake
+    )
+
   
   if( ${GMI_SIM_FOUND} )
-    set(CDIR ${CASES}/incompressibleAdapt)
-    cp_serial_test(copy_inpConfig_adapt_incompressible
-      cp ${PHASTA_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
-  
-    set(exe ${PHASTACHEF_BINARY_DIR}/chefPhastaLoop_stream_adapt)
-    set(maxTimeStep 8)
-    set(casename loopStreamAdapt_incompressible)
-    cp_parallel_test(${casename} 2 ${CDIR} ${exe} ${maxTimeStep})
-    cp_move_dir(${casename} ${CDIR} 2-procs_case)
-    cp_move_dir(${casename} ${CDIR} 4)
-    cp_move_dir(${casename} ${CDIR} 8)
-  
-    set(CDIR ${CASES}/incompressibleAdapt)
-    set(exe ${PHASTACHEF_BINARY_DIR}/chefPhastaLoop_sam_stream_adapt)
-    set(maxTimeStep 8)
-    set(casename loopStreamSamAdapt_incompressible)
-    cp_parallel_test(${casename} 2 ${CDIR} ${exe} ${maxTimeStep})
-    cp_move_dir(${casename} ${CDIR} 2-procs_case)
-    cp_move_dir(${casename} ${CDIR} 4)
-    cp_move_dir(${casename} ${CDIR} 8)
-  
-    set(exe ${PHASTACHEF_BINARY_DIR}/chefPhastaLoop_files_adapt)
-    set(casename loopFilesAdapt_incompressible)
-    cp_parallel_test(${casename} 2 ${CDIR} ${exe} ${maxTimeStep})
-    cp_move_dir(${casename} ${CDIR} 2-procs_case)
-    cp_move_dir(${casename} ${CDIR} 4)
+    set(casename ${testLabel}_loopStreamAdapt_incompressible)
+    add_test(NAME ${casename}
+      COMMAND ${CMAKE_COMMAND}
+      -DNAME=${casename}
+      -DWORKDIR=${CASES}/incompressibleAdapt
+      -DINPFILE=${PHASTA_SOURCE_DIR}/phSolver/common/input.config
+      -DMPIRUN=${MPIRUN}
+      -DMPIRUN_PROCFLAG=${MPIRUN_PROCFLAG}
+      -DEXE=${PHASTACHEF_BINARY_DIR}/chefPhastaLoop_stream_adapt
+      -DNUMPROCS=2
+      -DMAXSTEPS=8
+      -DSRCDIRS=2-procs_case$<SEMICOLON>4$<SEMICOLON>8
+      -P ${CMAKE_CURRENT_SOURCE_DIR}/runphasta.cmake
+      )
+
+    set(casename ${testLabel}_loopSamStreamAdapt_incompressible)
+    add_test(NAME ${casename}
+      COMMAND ${CMAKE_COMMAND}
+      -DNAME=${casename}
+      -DWORKDIR=${CASES}/incompressibleAdapt
+      -DINPFILE=${PHASTA_SOURCE_DIR}/phSolver/common/input.config
+      -DMPIRUN=${MPIRUN}
+      -DMPIRUN_PROCFLAG=${MPIRUN_PROCFLAG}
+      -DEXE=${PHASTACHEF_BINARY_DIR}/chefPhastaLoop_sam_stream_adapt
+      -DNUMPROCS=2
+      -DMAXSTEPS=8
+      -DSRCDIRS=2-procs_case$<SEMICOLON>4
+      -P ${CMAKE_CURRENT_SOURCE_DIR}/runphasta.cmake
+      )
+
+    set(casename ${testLabel}_loopFilesAdapt_incompressible)
+    add_test(NAME ${casename}
+      COMMAND ${CMAKE_COMMAND}
+      -DNAME=${casename}
+      -DWORKDIR=${CASES}/incompressibleAdapt
+      -DINPFILE=${PHASTA_SOURCE_DIR}/phSolver/common/input.config
+      -DMPIRUN=${MPIRUN}
+      -DMPIRUN_PROCFLAG=${MPIRUN_PROCFLAG}
+      -DEXE=${PHASTACHEF_BINARY_DIR}/chefPhastaLoop_files_adapt
+      -DNUMPROCS=2
+      -DMAXSTEPS=8
+      -DSRCDIRS=2-procs_case$<SEMICOLON>4
+      -P ${CMAKE_CURRENT_SOURCE_DIR}/runphasta.cmake
+      )
   endif()
-  endif()
+endif()
