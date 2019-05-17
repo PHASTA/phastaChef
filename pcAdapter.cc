433ac54f86a0fd07cadef8ac82874892da2699ad
Fan Yang <yangf4@rpi.edu>
2019-05-17 15:50:56 -0400

    prevent mesh size lower than 0.0002

--- pcAdapter.cc
+++ pcAdapter.cc
@@ -501,7 +501,8 @@ namespace pc {
     while ((v = m->iterate(vit))) {
       if(!vertexIsInCylinder(v)) continue;
       apf::getVector(sizes,v,0,v_mag);
-      if(v_mag[0] > 0.008) v_mag[0] = 0.008;
+      if(v_mag[0] > 0.008)  v_mag[0] = 0.008;
+      if(v_mag[0] < 0.0002) v_mag[0] = 0.0002;
       pVertex meshVertex = reinterpret_cast<pVertex>(v);
       MSA_setVertexSize(adapter, meshVertex, v_mag[0]);
     }
